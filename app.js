f0fd175b387c6e8599671a2d03da3aa6f358119a
Jorge Fernandez <fsjorgeluis@gmail.com>
2022-05-10 15:25:40 -0400

    feat: :sparkles: Static side addition for educative purposes

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const port = 4000;
 // Middleware Servir contenido estatico
 app.use(express.static('public'));
 
-app.get('*', function (req, res) {
+app.get('/generic', (req, res) => {
+	res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+	res.sendFile(__dirname + '/public/elements.html');
+});
+
+app.get('*', (req, res) => {
 	res.sendFile(__dirname + '/public/404.html');
 });
 
