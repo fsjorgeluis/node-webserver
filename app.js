dffd42344bc21c5ebdb4b09200249985c5265f35
Jorge Fernandez <fsjorgeluis@gmail.com>
2022-05-10 15:58:29 -0400

    feat: :construction: Crating partials for handlebars

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const port = 4000;
 
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 // Middleware Servir contenido estatico
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+	res.render('home', {
+		nombre: 'Jorge',
+		titulo: 'Curso de Node',
+	});
+});
+
 app.get('/generic', (req, res) => {
-	res.sendFile(__dirname + '/public/generic.html');
+	res.render('generic', {
+		nombre: 'Jorge',
+		titulo: 'Generic - Curso de Node',
+	});
 });
 
 app.get('/elements', (req, res) => {
-	res.sendFile(__dirname + '/public/elements.html');
+	res.render('elements', {
+		nombre: 'Jorge',
+		titulo: 'Elements - Curso de Node',
+	});
 });
 
 app.get('*', (req, res) => {
